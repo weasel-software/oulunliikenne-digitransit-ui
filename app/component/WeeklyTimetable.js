be27b844933847d2eaceb4b95f701947be337b3a
Joona Olkkola <joona.olkkola@octo3.fi>
2018-06-25 16:58:30 +0300

    Initial implementation of timetable printing.

---
+++ app/component/WeeklyTimetable.js
@@ -0,0 +1,214 @@
+import groupBy from 'lodash/groupBy';
+import isEmpty from 'lodash/isEmpty';
+import padStart from 'lodash/padStart';
+import trimStart from 'lodash/trimStart';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import CardHeader from './CardHeader';
+import Icon from './Icon';
+import * as sampleData from './data/WeeklyTimetable';
+
+class WeeklyTimetable extends React.Component {
+  dateForPrinting = (dateBegin, dateEnd) => (
+    <div className="printable-date-container">
+      <div className="printable-date-icon">
+        <Icon className="large-icon" img="icon-icon_schedule" />
+      </div>
+      <div className="printable-date-right">
+        <div className="printable-date-header">
+          <FormattedMessage id="date" defaultMessage="Date" />
+        </div>
+        <div className="printable-date-content">
+          {moment(dateBegin).format('dd DD.MM.YYYY')}&nbsp;-&nbsp;
+          {moment(dateEnd).format('dd DD.MM.YYYY')}
+        </div>
+      </div>
+    </div>
+  );
+
+  cleanRouteId = routeId =>
+    routeId ? trimStart(routeId.substring(1), '0') : '';
+
+  hasNote = departure => !isEmpty(departure.note);
+
+  hasNotes = (departuresArray, notes) =>
+    !isEmpty(notes) && departuresArray.some(this.hasNote);
+
+  renderHourlyRows = departuresArray => {
+    const departuresByHour = groupBy(
+      departuresArray.map(wd => ({
+        ...wd,
+        hourKey: wd.hours + (wd.isNextDay ? 24 : 0),
+      })),
+      wd => wd.hourKey,
+    );
+
+    return Object.keys(departuresByHour)
+      .sort((a, b) => a - b)
+      .map(hourKey => {
+        const departures = departuresByHour[hourKey];
+        const hour = padStart(departures[0] && departures[0].hours, 2, '0');
+        return (
+          <div className="timetable-row" key={hourKey}>
+            <h1 className="title bold">{hour}:</h1>
+            <div className="timetable-printable-title">{hour}</div>
+            <div className="timetable-rowcontainer">
+              {departures
+                .sort((a, b) => a.minutes - b.minutes)
+                .map(departure => (
+                  <div
+                    className="timetablerow-linetime"
+                    key={`${departure.minutes}-${departure.routeId}`}
+                  >
+                    <span>
+                      <div>
+                        <span className="bold">
+                          {padStart(departure.minutes, 2, '0')}
+                        </span>
+                        <span className="line-name">
+                          /{this.cleanRouteId(departure.routeId)}
+                          {this.hasNote(departure) && (
+                            <span className="bold">{departure.note}</span>
+                          )}
+                        </span>
+                      </div>
+                    </span>
+                  </div>
+                ))}
+            </div>
+          </div>
+        );
+      });
+  };
+
+  renderPeriodRows = (departuresArray, notes) => (
+    <div className="momentum-scroll">
+      <div className="timetable-time-headers">
+        <div className="hour">
+          <FormattedMessage id="hour" defaultMessage="Hour" />
+        </div>
+        <div className="minutes-per-route">
+          <FormattedMessage id="minutes-or-route" defaultMessage="Min/Route" />
+        </div>
+      </div>
+      {this.renderHourlyRows(departuresArray)}
+      {this.hasNotes(departuresArray, notes) && (
+        <div className="route-remarks">
+          <h1>
+            <FormattedMessage id="explanations" defaultMessage="Explanations" />:
+          </h1>
+          {notes.map(o => (
+            <div className="remark-row" key={`${o}`}>
+              <span>{o}</span>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+
+  render() {
+    const {
+      dateBegin,
+      dateEnd,
+      language,
+      notes,
+      saturdays,
+      sundays,
+      stopNameFi,
+      stopNameSe,
+      stopShortId,
+      weekdays,
+    } = this.props;
+
+    return (
+      <React.Fragment>
+        <CardHeader
+          code={stopShortId}
+          headingStyle="h1"
+          icon="icon-icon_bus-stop"
+          name={language === 'sv' ? stopNameSe : stopNameFi}
+          unlinked
+        />
+        <div className="timetable">
+          <div className="timetable-for-printing-header">
+            <h1>
+              <FormattedMessage id="timetable" defaultMessage="Timetable" />
+            </h1>
+          </div>
+          <div className="timetable-for-printing">
+            {this.dateForPrinting(dateBegin, dateEnd)}
+          </div>
+          <h2>
+            {moment()
+              .day(1)
+              .format('dddd')}&nbsp;-&nbsp;{moment()
+              .day(5)
+              .format('dddd')}
+          </h2>
+          {this.renderPeriodRows(weekdays, notes)}
+          <div className="page-break-after" />
+          <h2>
+            {moment()
+              .day(6)
+              .format('dddd')}
+          </h2>
+          {this.renderPeriodRows(saturdays, notes)}
+          <div className="page-break-after" />
+          <h2>
+            {moment()
+              .day(7)
+              .format('dddd')}
+          </h2>
+          {this.renderPeriodRows(sundays, notes)}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+const routeEntryShape = PropTypes.shape({
+  hours: PropTypes.number,
+  minutes: PropTypes.number,
+  note: PropTypes.string,
+  routeId: PropTypes.string,
+  isNextDay: PropTypes.bool,
+  dateBegin: PropTypes.string,
+  dateEnd: PropTypes.string,
+});
+
+WeeklyTimetable.propTypes = {
+  weekdays: PropTypes.arrayOf(routeEntryShape).isRequired,
+  saturdays: PropTypes.arrayOf(routeEntryShape).isRequired,
+  sundays: PropTypes.arrayOf(routeEntryShape).isRequired,
+  notes: PropTypes.arrayOf(PropTypes.string).isRequired,
+  dateBegin: PropTypes.string.isRequired,
+  dateEnd: PropTypes.string.isRequired,
+  language: PropTypes.oneOf('fi', 'sv', 'en'),
+  stopNameFi: PropTypes.string.isRequired,
+  stopNameSe: PropTypes.string.isRequired,
+  stopShortId: PropTypes.string.isRequired,
+};
+
+WeeklyTimetable.defaultProps = {
+  language: 'fi',
+};
+
+WeeklyTimetable.description = () => (
+  <React.Fragment>
+    <WeeklyTimetable {...sampleData.default.sample1} />
+    <div className="page-break-after" />
+    <WeeklyTimetable {...sampleData.default.sample2} />
+    <div className="page-break-after" />
+    <WeeklyTimetable {...sampleData.default.sample3} />
+    <div className="page-break-after" />
+    <WeeklyTimetable {...sampleData.default.sample4} />
+    <div className="page-break-after" />
+    <WeeklyTimetable {...sampleData.default.sample5} />
+  </React.Fragment>
+);
+
+export default WeeklyTimetable;
---
+++ app/component/data/WeeklyTimetable/index.js
@@ -0,0 +1,13 @@
+import * as sample1 from './1020120.json';
+import * as sample2 from './1250105.json';
+import * as sample3 from './1291501.json';
+import * as sample4 from './2222604.json';
+import * as sample5 from './4920206.json';
+
+export default {
+  sample1,
+  sample2,
+  sample3,
+  sample4,
+  sample5,
+};
