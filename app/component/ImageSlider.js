b7cb15284a30649cc4387b849210c8aeca6e8a56
Martin Roos <martin.roos@nordsoftware.com>
2019-01-16 16:41:05 +0200

    Improving CameraStation popup functionality

--- app/component/ImageSlider.js
+++ app/component/ImageSlider.js
@@ -1,67 +1,109 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import ComponentUsageExample from './ComponentUsageExample';
+import Icon from './Icon';
 
 class ImageSlider extends React.Component {
   static FIRST = 0;
 
-  constructor(props, context) {
+  constructor(props) {
     super(props);
     this.state = {
       active: ImageSlider.FIRST,
     };
   }
 
-  changeActive = (active) => {
+  changeActive = active => {
     this.setState({
-      active: active,
+      active,
     });
-  }
+  };
 
   next = () => {
     const { children } = this.props;
     const { active } = this.state;
-    const next = ((active + 1) === children.length) ? ImageSlider.FIRST : (active + 1);
+    const next =
+      active + 1 === children.length ? ImageSlider.FIRST : active + 1;
 
     this.setState({
       active: next,
     });
-  }
+  };
 
   prev = () => {
     const { children } = this.props;
     const { active } = this.state;
-    const prev = (active === ImageSlider.FIRST) ? (children.length - 1) : (active - 1);
+    const prev =
+      active === ImageSlider.FIRST ? children.length - 1 : active - 1;
 
     this.setState({
       active: prev,
     });
-  }
+  };
 
   render() {
     const { children } = this.props;
     const { active } = this.state;
 
     return (
-      <div className="image-slider">
+      <div
+        className="image-slider"
+        role="tablist"
+        tabIndex={-1}
+        onKeyDown={evt => {
+          if (evt.which === 37) {
+            this.prev();
+          } else if (evt.which === 39) {
+            this.next();
+          }
+        }}
+      >
         <div className="slides">
           {React.Children.map(children, (item, i) => {
-            const className = ((item.props.className || '') + (i === active ? ' active' : '')).trim();
-            const props = { ...item.props, className: className };
+            const className = (
+              (item.props.className || '') + (i === active ? ' active' : '')
+            ).trim();
+            const props = { ...item.props, className };
             return React.cloneElement(item, props);
           })}
         </div>
         <div className="nav">
           {React.Children.map(children, (item, i) => (
-            <span onClick={() => this.changeActive(i)} className={(i === active ? 'active' : '')}></span>
+            <span
+              onClick={() => this.changeActive(i)}
+              onKeyDown={evt => {
+                if (evt.which === 32) {
+                  this.changeActive(i);
+                }
+              }}
+              className={i === active ? 'active' : ''}
+              role="button"
+              tabIndex={i + 1}
+            />
           ))}
         </div>
-        <div className="prev" onClick={this.prev}></div>
-        <div className="next" onClick={this.next}></div>
+        <div
+          className="prev"
+          onClick={this.prev}
+          onKeyDown={() => {}}
+          role="button"
+          tabIndex={-1}
+        >
+          <Icon img="icon-icon_arrow-collapse--left" />
+        </div>
+        <div
+          className="next"
+          onClick={this.next}
+          onKeyDown={() => {}}
+          role="button"
+          tabIndex={-1}
+        >
+          <Icon img="icon-icon_arrow-collapse--right" />
+        </div>
       </div>
-    )
+    );
   }
-};
+}
 
 ImageSlider.displayName = 'ImageSlider';
 
@@ -70,8 +112,8 @@ ImageSlider.description = (
     <p>Image slider wrapper</p>
     <ComponentUsageExample description="">
       <ImageSlider>
-        <img src="" alt=""/>
-        <img src="" alt=""/>
+        <img src="" alt="" />
+        <img src="" alt="" />
       </ImageSlider>
     </ComponentUsageExample>
   </div>
